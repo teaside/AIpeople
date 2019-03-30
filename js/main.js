347ccdaad32cc9f68793382d338ce974a22f2d46
rDim <dima.rykunov@outlook.com>
2019-03-30 23:32:50 +0300

    (new) define scroll current position

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 window.addEventListener('DOMContentLoaded', function () {
 
     let overlay = document.getElementById('overlay');
+    var scroll = document.querySelector('.scroll-block__progress-bar');
     let body = document.getElementsByTagName('body')[0];
     let scrollFromTop = '0px';
     document.getElementById('openMenuBtn').addEventListener('click', () => {
@@ -15,4 +16,59 @@ window.addEventListener('DOMContentLoaded', function () {
         body.classList.remove('noscroll');
     });
 
+    window.addEventListener('scroll', function () {
+        setScroll(scroll);
+    });
+
+    // var color = document.querySelector('.scroll-block__progress-bar');
+    // console.log(color.);
 });
+
+// When the user scrolls the page, execute myFunction 
+// window.onscroll = function() {myFunction()};
+
+function setScroll(scroll) {
+  const winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  const height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  const scrolled = (winScroll / height) * 100;
+  if(scrolled <= 25 && !scroll.classList.contains("scroll-block__progress-bar_s1")) {
+    if (scroll.classList.contains("scroll-block__progress-bar_s3"))
+    scroll.classList.remove("scroll-block__progress-bar_s3");
+    if (scroll.classList.contains("scroll-block__progress-bar_s4"))
+    scroll.classList.remove("scroll-block__progress-bar_s4");
+    if (scroll.classList.contains("scroll-block__progress-bar_s2"))
+    scroll.classList.remove("scroll-block__progress-bar_s2");
+    
+    scroll.classList.add("scroll-block__progress-bar_s1");
+  } else
+  if((scrolled >=26 && scrolled <= 50) && !scroll.classList.contains("scroll-block__progress-bar_s2") ) {
+    if (scroll.classList.contains("scroll-block__progress-bar_s1"))
+    scroll.classList.remove("scroll-block__progress-bar_s1");
+    if (scroll.classList.contains("scroll-block__progress-bar_s3"))
+    scroll.classList.remove("scroll-block__progress-bar_s3");
+    if (scroll.classList.contains("scroll-block__progress-bar_s4"))
+    scroll.classList.remove("scroll-block__progress-bar_s4");
+    
+    scroll.classList.add("scroll-block__progress-bar_s2");
+  } else
+  if((scrolled >=51 && scrolled <= 75) && !scroll.classList.contains("scroll-block__progress-bar_s3") ) {
+    if (scroll.classList.contains("scroll-block__progress-bar_s1"))
+    scroll.classList.remove("scroll-block__progress-bar_s1");
+    if (scroll.classList.contains("scroll-block__progress-bar_s2"))
+    scroll.classList.remove("scroll-block__progress-bar_s2");
+    if (scroll.classList.contains("scroll-block__progress-bar_s4"))
+    scroll.classList.remove("scroll-block__progress-bar_s4");
+    
+    scroll.classList.add("scroll-block__progress-bar_s3");
+  } else
+  if((scrolled >=76 && scrolled <= 100) && !scroll.classList.contains("scroll-block__progress-bar_s4" )) {
+    if (scroll.classList.contains("scroll-block__progress-bar_s1"))
+    scroll.classList.remove("scroll-block__progress-bar_s1");
+    if (scroll.classList.contains("scroll-block__progress-bar_s2"))
+    scroll.classList.remove("scroll-block__progress-bar_s2");
+    if (scroll.classList.contains("scroll-block__progress-bar_s3"))
+    scroll.classList.remove("scroll-block__progress-bar_s3");
+    
+    scroll.classList.add("scroll-block__progress-bar_s4");
+  }
+}
